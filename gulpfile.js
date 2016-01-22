2c13c581e117a3b45a020185cf8273e62fd64bbe
MrLokans <trikster1911@gmail.com>
2016-01-22 20:40:03 +0300

    Bootstrap fonts fixed.

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,13 @@ gulp.task('default', ['copy']);
 gulp.task('copy', function() {
     // copy files
     gulp.src(['bower_components/bootstrap/dist/js/bootstrap.min.js', 'bower_components/jquery/dist/jquery.min.js'])
-        .pipe(gulp.dest('js/vendor/'))
+        .pipe(gulp.dest('js/vendor/'));
     gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css'])
-        .pipe(gulp.dest('css/vendor/'))
+        .pipe(gulp.dest('css/vendor/'));
+    gulp.src(['bower_components/bootstrap/dist/fonts/*.*'])
+        .pipe(gulp.dest('css/fonts/'));
 });
 
 gulp.task('watch', function(){
     gulp.watch('css/*.css', ['default']);
-})
+});
