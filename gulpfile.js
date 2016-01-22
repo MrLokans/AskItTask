890b64b632a11676ad0241ec2282143e4b309c0f
MrLokans <trikster1911@gmail.com>
2016-01-22 15:07:38 +0300

    gulp basic settings added.

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+var gulp = require('gulp');
+
+gulp.task('default', ['copy']);
+
+gulp.task('copy', function() {
+    // copy files
+    gulp.src(['bower_components/bootstrap/dist/js/bootstrap.min.js', 'bower_components/jquery/dist/jquery.min.js'])
+        .pipe(gulp.dest('js/vendor/'))
+    gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css'])
+        .pipe(gulp.dest('css/vendor/'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch('css/*.css', ['default']);
+})
