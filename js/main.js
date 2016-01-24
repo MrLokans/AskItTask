c6a1b122392d7f70578ff433837989023c70ea45
Fuckingnullpointerexception <darienyry@gmail.com>
2016-01-25 00:25:59 +0300

    Fixed TO-DO list in small size. Closes #13.

--- js/main.js
+++ js/main.js
@@ -19,9 +19,9 @@ var addToDo = function(){
     if(taskString !== ''){
         var taskList = $('#task-list');
 
-        var taskEntry = $('<li class="row task-entry"></li>');
-        var checkedColumn = $('<input type="checkbox" class="col-xs-1">');         
-        var titleColumn = $('<div class="task-title col-xs-10">' + taskString + '</div>');
+        var taskEntry = $('<li class="task-entry"></li>');
+        var checkedColumn = $('<input type="checkbox" class="col-xs-2 col-md-1 col-sm-1">');         
+        var titleColumn = $('<div class="task-title col-xs-8 col-md-10 col-sm-10">' + taskString + '</div>');
         
         checkedColumn.change(function(e){
             titleColumn.toggleClass('deleted-text');
