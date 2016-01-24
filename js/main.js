c88bb37ad3d67b49d2196e435057443b8c586392
bitreight <alexey.kur@gmail.com>
2016-01-24 18:45:06 +0300

    Extra comment added

--- js/main.js
+++ js/main.js
@@ -36,8 +36,11 @@ $(document).ready(function(){
         
         else {  
             $("#alertEmptyField").removeClass('hide');
-            setTimeout(function(){$("#alertEmptyField").hide(250);}, 1500);
-            $("#alertEmptyField").show(0);                
+            setTimeout(function(){
+                $("#alertEmptyField").hide(250);
+            }, 1500);
+            // show method is called BEFORE the previous .hide() method. 
+            $("#alertEmptyField").show(0);                 
         }
     });
 
