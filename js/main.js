8f8a872d9e348a4455775d3f1035d4507bd301e5
bitreight <alexey.kur@gmail.com>
2016-01-24 19:41:50 +0300

    Task now is submitted by pressing enter key. closes #4

--- js/main.js
+++ js/main.js
@@ -1,50 +1,59 @@
 $(document).ready(function(){
     var createTaskButton = $('#createTask');
+    var inputText = $('input.todo-input-text');
 
-    createTaskButton.on('click', function(e){
-        taskInput = $('.todo-input-text');
-        taskString = taskInput.val();
-        
-        if(taskString !== ''){
-            var taskList = $('#task-list');
-
-            var taskEntry = $('<li class="row task-entry"></li>');
-            var checkedColumn = $('<input type="checkbox" class="col-xs-1">');         
-            var titleColumn = $('<div class="task-title col-xs-10">' + taskString + '</div>');
-            
-            checkedColumn.change(function(e){
-                titleColumn.toggleClass('deleted-text');
-            });
-            
-            var deleteColumn = $('<span class="glyphicon glyphicon-trash pull-right task-delete"></span>');
-
-            deleteColumn.on('click', function(e){
-                deleteTarget = deleteColumn.parent();
-                deleteTarget.hide(250, function(){
-                    deleteTarget.remove();
-                });
-            });
-
-            taskEntry.append(checkedColumn);
-            taskEntry.append(titleColumn);
-            taskEntry.append(deleteColumn);
-
-            taskList.append(taskEntry);
-
-            clearInput(taskInput);
-        } 
-        
-        else {  
-            $("#alertEmptyField").removeClass('hide');
-            setTimeout(function(){
-                $("#alertEmptyField").hide(250);
-            }, 1500);
-            // show method is called BEFORE the previous .hide() method. 
-            $("#alertEmptyField").show(0);                 
+    inputText.on('keydown', function(event){
+        if(event.which == 13){
+            addToDo();
         }
     });
 
+    createTaskButton.on('click', addToDo);
+
 });
+
+var addToDo = function(){
+    taskInput = $('.todo-input-text');
+    taskString = taskInput.val();
+    
+    if(taskString !== ''){
+        var taskList = $('#task-list');
+
+        var taskEntry = $('<li class="row task-entry"></li>');
+        var checkedColumn = $('<input type="checkbox" class="col-xs-1">');         
+        var titleColumn = $('<div class="task-title col-xs-10">' + taskString + '</div>');
+        
+        checkedColumn.change(function(e){
+            titleColumn.toggleClass('deleted-text');
+        });
+        
+        var deleteColumn = $('<span class="glyphicon glyphicon-trash pull-right task-delete"></span>');
+
+        deleteColumn.on('click', function(e){
+            deleteTarget = deleteColumn.parent();
+            deleteTarget.hide(250, function(){
+                deleteTarget.remove();
+            });
+        });
+
+        taskEntry.append(checkedColumn);
+        taskEntry.append(titleColumn);
+        taskEntry.append(deleteColumn);
+
+        taskList.append(taskEntry);
+
+        clearInput(taskInput);
+    } 
+    
+    else {  
+        $("#alertEmptyField").removeClass('hide');
+        setTimeout(function(){
+            $("#alertEmptyField").hide(250);
+        }, 1500);
+        // show method is called BEFORE the previous .hide() method. 
+        $("#alertEmptyField").show(0);                 
+    }
+}
 
 var clearInput = function(input){
     input.val('');
