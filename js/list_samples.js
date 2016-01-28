0cd5fdc283b22fcfc7c9809db2a0a62e8853ddc7
MrLokans <trikster1911@gmail.com>
2016-01-28 14:03:17 +0300

    List JS-data validated. Script with list data now added to the page.

--- js/list_samples.js
+++ js/list_samples.js
@@ -1,53 +1,43 @@
 todo_data = {
     "lists": [
-    {
-        "list_id": 1,
-        "list_title": "Test List 1",
-        "todo_count": 1,
-        "todos": [
-            {
-                "todo_id": 100,
-                "todo_title": "My Test To-Do",
-                "todo_tags": [
-                    {
-                        "title": "My Tag"
-                    }
-                ],
-                "todo_complete_date": "12:20:23-28.04.2015"
-            },
-            {
-                "todo_id": 101,
-                "todo_title": "My Test To-Do #2",
-                "todo_tags": [
-                ],
-                "todo_complete_date": "13:17:29-01.11.2015"
-            },
-        ]
-    },
-    {
-        "list_id": 2,
-        "list_title": "Test List 2",
-        "todo_count": 2,
-        "todos": [
-            {
-                "todo_id": 120,
-                "todo_title": "This is a to-do in another list",
-                "todo_tags": [
-                    {
-                        "title": "My Tag"
-                    }
-                ],
-                "todo_complete_date": "12:20:23-28.04.2015"
-            },
-            {
-                "todo_id": 121,
-                "todo_title": "This is a to-do in another list #2",
-                "todo_tags": [
-                ],
-                "todo_complete_date": "13:17:29-01.11.2015"
-            },
-        ]
-    }
+        {
+            "list_id": 1,
+            "list_title": "Test List 1",
+            "todo_count": 1,
+            "todos": [
+                {
+                    "todo_id": 100,
+                    "todo_title": "My Test To-Do",
+                    "todo_tags": [{"title": "My Tag"}],
+                    "todo_complete_date": "12:20:23-28.04.2015"
+                },
+                {
+                    "todo_id": 101,
+                    "todo_title": "My Test To-Do #2",
+                    "todo_tags": [],
+                    "todo_complete_date": "13:17:29-01.11.2015"
+                }
+            ]
+        },
+        {
+            "list_id": 2,
+            "list_title": "Test List 2",
+            "todo_count": 2,
+            "todos": [
+                {
+                    "todo_id": 120,
+                    "todo_title": "This is a to-do in another list",
+                    "todo_tags": [  {"title": "My Tag"} ],
+                    "todo_complete_date": "12:20:23-28.04.2015"
+                },
+                {
+                    "todo_id": 121,
+                    "todo_title": "This is a to-do in another list #2",
+                    "todo_tags": [],
+                    "todo_complete_date": "13:17:29-01.11.2015"
+                }
+            ]
+        }
 
     ]
 }
