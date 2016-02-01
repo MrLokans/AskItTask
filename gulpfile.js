3d8ea5352f14b5b5a9ec3bbe34d63b672bb39c5d
MrLokans <trikster1911@gmail.com>
2016-02-01 20:11:44 +0300

    Angular added as dependency.

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,8 @@ gulp.task('copy', function() {
     // copy files
     gulp.src(['bower_components/bootstrap/dist/js/bootstrap.min.js', 'bower_components/jquery/dist/jquery.min.js'])
         .pipe(gulp.dest('js/vendor/'));
+    gulp.src(['bower_components/angular/angular.min.js'])
+        .pipe(gulp.dest('js/vendor/'));
     gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css'])
         .pipe(gulp.dest('css/vendor/'));
     gulp.src(['bower_components/bootstrap/dist/fonts/*.*'])
